e0d7270b3ddb10a791077baa72a953d010dec242
Rica Mae Jin C. Calugtong <ricamaejinccalugtong@iskolarngbayan.pup.edu.ph>
2025-10-01 20:37:52 +0800

    view profile

--- frontend/js/registration_config.js
+++ frontend/js/registration_config.js
@@ -3,3 +3,5 @@ window.registrationConfig = {
     hoaMemberTitle: "HOA Member Registration",
     familyOfHoaMemberTitle: "Family of an HOA Member Registration"
 };
+
+/*Connected to the vire profile para makuha yung mga title card */
