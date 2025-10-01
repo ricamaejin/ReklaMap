67d0bab7c0887ec06ed0e84eda880d2b458bf079
Rica Mae Jin C. Calugtong <ricamaejinccalugtong@iskolarngbayan.pup.edu.ph>
2025-10-01 20:32:01 +0800

    Fix: update view profile page and add JS config

---
+++ frontend/js/registration_config.js
@@ -0,0 +1,5 @@
+window.registrationConfig = {
+    nonMemberTitle: "Non-HOA Member Registration",
+    hoaMemberTitle: "HOA Member Registration",
+    familyOfHoaMemberTitle: "Family of an HOA Member Registration"
+};
